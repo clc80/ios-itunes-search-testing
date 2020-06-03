09379ea59574a07674520e7484e854b9dd21925a
Claudia Maciel <clc_80@yahoo.com>
2020-06-02 19:58:55 -0700

    Created a test to test the datatask

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -7,27 +7,26 @@
 //
 
 import XCTest
+@testable import iTunes_Search
 
 class iTunes_SearchTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testSucessfulSearchResults() {
+        
+        let searchResultsController = SearchResultController()
+        
+        let expectation = self.expectation(description: "Waiting for iTunes API")
+        searchResultsController.performSearch(for: "Candy Crush", resultType: .software) { (result) in
+            switch result {
+            case .success(let searchResultsArray):
+                XCTAssert(searchResultsArray.count > 0)
+               break
+            case .failure(let error):
+                XCTFail("The iTunes API failed \(error)")
+            }
+            expectation.fulfill()
         }
+         wait(for: [expectation], timeout: 5)
     }
 
 }
